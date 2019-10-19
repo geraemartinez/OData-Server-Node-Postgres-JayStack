5832b106b87f64113ab446ecbbca74f6c3e5c911
Gerardo Elisiel Martínez Cabral <gerae.martinez@gmail.com>
2019-10-19 00:52:03 -0500

    Project Fixed to use Typescript

--- app.ts
+++ app.ts
@@ -3,20 +3,58 @@ import * as path  from 'path';
 import cookieParser from 'cookie-parser';
 import logger  from 'morgan';
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import IndexController from './routes/IndexController';
+import { EcomServer } from "./server";
+import { Router, Request, Response, NextFunction } from 'express';
+import errorMiddleware from './middleware/error.middleware';
+
+class Application  {
+
+    public app: express.Application;
+    public port: number;
+    public oData: express.Router;
 
 
-const app: express.Application = express();
+    constructor(controllers, port) {
+        this.app = express();
+        this.oData = EcomServer.create();
+        this.port = port;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+        this.initializeMiddlewares();
+        this.initializeControllers(controllers);
+        this.initializeODataController();
+        this.initializeErrorHandling();
+    }
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    private initializeMiddlewares() {
+        this.app.use(logger('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(cookieParser());
+        this.app.use(express.static(path.join(__dirname, 'public')));
+    }
+
+    private initializeControllers(controllers) {
+        controllers.forEach((controller) => {
+          this.app.use('/', controller.router);
+        });
+    }
+
+    public initializeODataController(){
+        this.app.use('/oData', this.oData);
+    }
 
 
-export default app;
+    public listen() {
+        this.app.listen(this.port, () => {
+          console.log(`App listening on the port ${this.port}`);
+        });
+    }
+
+    private initializeErrorHandling() {
+        this.app.use(errorMiddleware);
+    }
+   
+}
+export default Application;
+
